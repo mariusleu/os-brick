9de625570de34127a286ee0dfe5aeb08e50402d3
Marius Leustean <marius.leus@gmail.com>
2019-12-18 15:58:40 +0200

    copy functions from cinder to see how import works

--- os_brick/initiator/connectors/vmware.py
+++ os_brick/initiator/connectors/vmware.py
@@ -18,6 +18,7 @@ import tempfile
 
 from oslo_log import log as logging
 from oslo_utils import fileutils
+from oslo_utils import units
 
 try:
     from oslo_vmware import api
@@ -35,6 +36,9 @@ from os_brick.i18n import _
 from os_brick.initiator import initiator_connector
 
 LOG = logging.getLogger(__name__)
+
+BACKING_UUID_KEY = 'instanceUuid'
+MIN_VIRTUAL_DISK_SIZE_KB = 4 * units.Ki
 
 
 class VmdkConnector(initiator_connector.InitiatorConnector):
@@ -193,8 +197,8 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
         image_transfer._start_transfer(read_handle, write_handle, timeout_secs)
         return write_handle.get_imported_vm()
 
-    def _disconnect(self, tmp_file_path, session, volume, volume_id, rp_ref,
-                    vm_folder_ref, import_spec, vmdk_size):
+    def _disconnect(self, tmp_file_path, session, volume, rp_ref,
+                    vm_folder_ref, **kwargs):
 
         volume_ops = VolumeOps(session=session)
         #
@@ -218,6 +222,17 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
             #     vim_util, 'get_object_property', session.vim, dc_ref, 'name')
             # cookies = session.vim.client.options.transport.cookiejar
             # cacerts = self._ca_file if self._ca_file else not self._insecure
+            vm_config_spec = volume_ops.get_create_spec(
+                name=kwargs.get('name'),
+                size_kb=kwargs.get('size_kb'),
+                disk_type=kwargs.get('disk_type'),
+                ds_name=kwargs.get('ds_name'),
+                profile_id=kwargs.get('profile_id'),
+                adapter_type=kwargs.get('adapter_type'),
+                extra_config=kwargs.get('extra_config'))
+
+            vm_import = volume_ops.import_spec(config_spec=vm_config_spec)
+
             imported_vm = self._upload_vmdk(tmp_file,
                                             self._ip,
                                             self._port,
@@ -225,10 +240,11 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
                                             session,
                                             rp_ref,
                                             vm_folder_ref,
-                                            import_spec,
-                                            vmdk_size)
+                                            vm_import,
+                                            kwargs.get('vmdk_size'))
             volume_ops.delete_backing(volume)
-            volume_ops.update_backing_disk_uuid(imported_vm, volume_id)
+            volume_ops.update_backing_disk_uuid(imported_vm, kwargs.get(
+                'volume_id'))
 
         # # Delete the current volume vmdk because the copy operation does not
         # # overwrite.
@@ -297,16 +313,13 @@ class VmdkConnector(initiator_connector.InitiatorConnector):
                 # dc_ref = vim_util.get_moref(
                 #     connection_properties['datacenter'], "Datacenter")
                 # vmdk_path = connection_properties['vmdk_path']
+                rp_ref = vim_util.get_moref(connection_properties['rp_ref'],
+                                            'ResourcePool')
+                vm_folder_ref = vim_util.get_moref(connection_properties[
+                                           'vm_folder_ref'], 'Folder')
                 self._disconnect(
-                    tmp_file_path, session,
-                    connection_properties['volume'],
-                    connection_properties['volume_id'],
-                    vim_util.get_moref(connection_properties['rp_ref'],
-                                       'ResourcePool'),
-                    vim_util.get_moref(connection_properties[
-                                           'vm_folder_ref'], 'Folder'),
-                    connection_properties['import_spec'],
-                    connection_properties['vmdk_size'])
+                    tmp_file_path, session,backing,
+                    rp_ref, vm_folder_ref, **connection_properties)
         finally:
             os.remove(tmp_file_path)
             if session:
@@ -431,6 +444,194 @@ class VolumeOps:
             disk_spec.profile = [disk_profile]
         return disk_spec
 
+    def _create_specs_for_disk_add(self, size_kb, disk_type, adapter_type,
+                                   profile_id, vmdk_ds_file_path=None):
+        """Create controller and disk config specs for adding a new disk.
+
+        :param size_kb: disk size in KB
+        :param disk_type: disk provisioning type
+        :param adapter_type: disk adapter type
+        :param profile_id: storage policy profile identification
+        :param vmdk_ds_file_path: Optional datastore file path of an existing
+                                  virtual disk. If specified, file backing is
+                                  not created for the virtual disk.
+        :return: list containing controller and disk config specs
+        """
+        controller_spec = None
+        if adapter_type == 'ide':
+            # For IDE disks, use one of the default IDE controllers (with keys
+            # 200 and 201) created as part of backing VM creation.
+            controller_key = 200
+        else:
+            controller_spec = self._create_controller_config_spec(adapter_type)
+            controller_key = controller_spec.device.key
+
+        disk_spec = self._create_virtual_disk_config_spec(size_kb,
+                                                          disk_type,
+                                                          controller_key,
+                                                          profile_id,
+                                                          vmdk_ds_file_path)
+        specs = [disk_spec]
+        if controller_spec is not None:
+            specs.append(controller_spec)
+        return specs
+
+    def _create_backing_int(self, folder, resource_pool, host, create_spec):
+        """Helper for create backing methods."""
+        LOG.debug("Creating volume backing with spec: %s.", create_spec)
+        task = self._session.invoke_api(self._session.vim, 'CreateVM_Task',
+                                        folder, config=create_spec,
+                                        pool=resource_pool, host=host)
+        task_info = self._session.wait_for_task(task)
+        backing = task_info.result
+        LOG.info("Successfully created volume backing: %s.", backing)
+        return backing
+
+    def _create_controller_config_spec(self, adapter_type):
+        """Returns config spec for adding a disk controller."""
+        cf = self._session.vim.client.factory
+
+        controller_type = ControllerType.get_controller_type(adapter_type)
+        controller_device = cf.create('ns0:%s' % controller_type)
+        controller_device.key = -100
+        controller_device.busNumber = 0
+        if ControllerType.is_scsi_controller(controller_type):
+            controller_device.sharedBus = 'noSharing'
+
+        controller_spec = cf.create('ns0:VirtualDeviceConfigSpec')
+        controller_spec.operation = 'add'
+        controller_spec.device = controller_device
+        return controller_spec
+
+    def import_spec(self, config_spec):
+        vm_import_spec = self._client_factory.create(
+            'ns0:VirtualMachineImportSpec')
+        vm_import_spec.configSpec = config_spec
+        return vm_import_spec
+
+    def config_spec(self, **kwargs):
+        cf = self._session.vim.client.factory
+        vm_file_info = cf.create('ns0:VirtualMachineFileInfo')
+        vm_file_info.vmPathName = '[%s]' % kwargs.get('ds_name')
+
+        create_spec = cf.create('ns0:VirtualMachineConfigSpec')
+        create_spec.name = kwargs.get('name') or kwargs.get('volume_name')
+        create_spec.guestId = 'otherGuest'
+        create_spec.numCPUs = 1
+        create_spec.memoryMB = 128
+        create_spec.files = vm_file_info
+        # Set the default hardware version to a compatible version supported by
+        # vSphere 5.0. This will ensure that the backing VM can be migrated
+        # without any incompatibility issues in a mixed cluster of ESX hosts
+        # with versions 5.0 or above.
+        create_spec.version = kwargs.get('vmx_version')
+        BACKING_UUID_KEY = 'instanceUuid'
+        profile_id = kwargs.get('profile_id')
+        if profile_id:
+            vm_profile = cf.create('ns0:VirtualMachineDefinedProfileSpec')
+            vm_profile.profileId = profile_id
+            create_spec.vmProfile = [vm_profile]
+
+        extra_config = kwargs.get('extra_config')
+        if extra_config:
+            if BACKING_UUID_KEY in extra_config:
+                create_spec.instanceUuid = extra_config.pop(BACKING_UUID_KEY)
+            create_spec.extraConfig = self._get_extra_config_option_values(
+                extra_config)
+
+        create_spec.managedBy = self._create_managed_by_info(kwargs.get(
+            'extension_key'), kwargs.get('extension_type'))
+
+        return create_spec
+
+    def _get_extra_config_option_values(self, extra_config):
+
+        cf = self._session.vim.client.factory
+        option_values = []
+
+        for key, value in extra_config.items():
+            opt = cf.create('ns0:OptionValue')
+            opt.key = key
+            opt.value = value
+            option_values.append(opt)
+
+        return option_values
+
+    def _create_managed_by_info(self, extension_key, extension_type):
+        managed_by = self._session.vim.client.factory.create(
+            'ns0:ManagedByInfo')
+        managed_by.extensionKey = extension_key
+        managed_by.type = extension_type
+        return managed_by
+
+    def _create_virtual_disk_config_spec(self, size_kb, disk_type,
+                                         controller_key, profile_id,
+                                         vmdk_ds_file_path):
+        """Returns config spec for adding a virtual disk."""
+        cf = self._session.vim.client.factory
+
+        disk_device = cf.create('ns0:VirtualDisk')
+        # disk size should be at least 4MB for VASA provider
+        min_size_kb = MIN_VIRTUAL_DISK_SIZE_KB
+        disk_device.capacityInKB = max(min_size_kb, int(size_kb))
+        if controller_key < 0:
+            disk_device.key = controller_key - 1
+        else:
+            disk_device.key = -101
+        disk_device.unitNumber = 0
+        disk_device.controllerKey = controller_key
+        disk_device.backing = self._create_disk_backing(disk_type,
+                                                        vmdk_ds_file_path)
+
+        disk_spec = cf.create('ns0:VirtualDeviceConfigSpec')
+        disk_spec.operation = 'add'
+        if vmdk_ds_file_path is None:
+            disk_spec.fileOperation = 'create'
+        disk_spec.device = disk_device
+        if profile_id:
+            disk_profile = cf.create('ns0:VirtualMachineDefinedProfileSpec')
+            disk_profile.profileId = profile_id
+            disk_spec.profile = [disk_profile]
+
+        return disk_spec
+
+    def _create_disk_backing(self, disk_type, vmdk_ds_file_path):
+        """Creates file backing for virtual disk."""
+        cf = self._session.vim.client.factory
+        disk_device_bkng = cf.create('ns0:VirtualDiskFlatVer2BackingInfo')
+
+        if disk_type == VirtualDiskType.EAGER_ZEROED_THICK:
+            disk_device_bkng.eagerlyScrub = True
+        elif disk_type == VirtualDiskType.THIN:
+            disk_device_bkng.thinProvisioned = True
+
+        disk_device_bkng.fileName = vmdk_ds_file_path or ''
+        disk_device_bkng.diskMode = 'persistent'
+
+        return disk_device_bkng
+
+    def get_create_spec(self, name, size_kb, disk_type, ds_name,
+                        profile_id=None, adapter_type='lsiLogic',
+                        extra_config=None):
+        """Return spec for creating backing with a single disk.
+
+        :param name: name of the backing
+        :param size_kb: disk size in KB
+        :param disk_type: disk provisioning type
+        :param ds_name: datastore name where the disk is to be provisioned
+        :param profile_id: storage policy profile identification
+        :param adapter_type: disk adapter type
+        :param extra_config: key-value pairs to be written to backing's
+                             extra-config
+        :return: spec for creation
+        """
+        create_spec = self.config_spec(
+            name=name, ds_name=ds_name, profile_id=profile_id,
+            extra_config=extra_config)
+        create_spec.deviceChange = self._create_specs_for_disk_add(
+            size_kb, disk_type, adapter_type, profile_id)
+        return create_spec
+
     def reconfig_vm(self, backing, reconfig_spec):
         LOG.debug("Reconfiguring backing VM: %(backing)s with spec: %(spec)s.",
                   {'backing': backing,
@@ -461,3 +662,148 @@ class VolumeOps:
             instanceUuid=True)
         if result:
             return result[0]
+
+class VirtualDiskType(object):
+    """Supported virtual disk types."""
+
+    EAGER_ZEROED_THICK = "eagerZeroedThick"
+    PREALLOCATED = "preallocated"
+    THIN = "thin"
+
+    # thick in extra_spec means lazy-zeroed thick disk
+    EXTRA_SPEC_DISK_TYPE_DICT = {'eagerZeroedThick': EAGER_ZEROED_THICK,
+                                 'thick': PREALLOCATED,
+                                 'thin': THIN
+                                 }
+
+    @staticmethod
+    def is_valid(extra_spec_disk_type):
+        """Check if the given disk type in extra_spec is valid.
+
+        :param extra_spec_disk_type: disk type in extra_spec
+        :return: True if valid
+        """
+        return (extra_spec_disk_type in
+                VirtualDiskType.EXTRA_SPEC_DISK_TYPE_DICT)
+
+    @staticmethod
+    def validate(extra_spec_disk_type):
+        """Validate the given disk type in extra_spec.
+
+        This method throws an instance of InvalidDiskTypeException if the given
+        disk type is invalid.
+
+        :param extra_spec_disk_type: disk type in extra_spec
+        :raises: InvalidDiskTypeException
+        """
+        if not VirtualDiskType.is_valid(extra_spec_disk_type):
+            raise ValueError()
+
+    @staticmethod
+    def get_virtual_disk_type(extra_spec_disk_type):
+        """Return disk type corresponding to the extra_spec disk type.
+
+        :param extra_spec_disk_type: disk type in extra_spec
+        :return: virtual disk type
+        :raises: InvalidDiskTypeException
+        """
+        VirtualDiskType.validate(extra_spec_disk_type)
+        return (VirtualDiskType.EXTRA_SPEC_DISK_TYPE_DICT[
+                extra_spec_disk_type])
+
+
+class VirtualDiskAdapterType(object):
+    """Supported virtual disk adapter types."""
+
+    LSI_LOGIC = "lsiLogic"
+    BUS_LOGIC = "busLogic"
+    LSI_LOGIC_SAS = "lsiLogicsas"
+    PARA_VIRTUAL = "paraVirtual"
+    IDE = "ide"
+
+    @staticmethod
+    def is_valid(adapter_type):
+        """Check if the given adapter type is valid.
+
+        :param adapter_type: adapter type to check
+        :return: True if valid
+        """
+        return adapter_type in [VirtualDiskAdapterType.LSI_LOGIC,
+                                VirtualDiskAdapterType.BUS_LOGIC,
+                                VirtualDiskAdapterType.LSI_LOGIC_SAS,
+                                VirtualDiskAdapterType.PARA_VIRTUAL,
+                                VirtualDiskAdapterType.IDE]
+
+    @staticmethod
+    def validate(extra_spec_adapter_type):
+        """Validate the given adapter type in extra_spec.
+
+        This method throws an instance of InvalidAdapterTypeException if the
+        given adapter type is invalid.
+
+        :param extra_spec_adapter_type: adapter type in extra_spec
+        :raises: InvalidAdapterTypeException
+        """
+        if not VirtualDiskAdapterType.is_valid(extra_spec_adapter_type):
+            raise ValueError()
+
+    @staticmethod
+    def get_adapter_type(extra_spec_adapter):
+        """Get the adapter type to be used in VirtualDiskSpec.
+
+        :param extra_spec_adapter: adapter type in the extra_spec
+        :return: adapter type to be used in VirtualDiskSpec
+        """
+        VirtualDiskAdapterType.validate(extra_spec_adapter)
+        # We set the adapter type as lsiLogic for lsiLogicsas/paraVirtual
+        # since it is not supported by VirtualDiskManager APIs. This won't
+        # be a problem because we attach the virtual disk to the correct
+        # controller type and the disk adapter type is always resolved using
+        # its controller key.
+        if (extra_spec_adapter == VirtualDiskAdapterType.LSI_LOGIC_SAS or
+                extra_spec_adapter == VirtualDiskAdapterType.PARA_VIRTUAL):
+            return VirtualDiskAdapterType.LSI_LOGIC
+        else:
+            return extra_spec_adapter
+
+
+class ControllerType(object):
+    """Encapsulate various controller types."""
+
+    LSI_LOGIC = 'VirtualLsiLogicController'
+    BUS_LOGIC = 'VirtualBusLogicController'
+    LSI_LOGIC_SAS = 'VirtualLsiLogicSASController'
+    PARA_VIRTUAL = 'ParaVirtualSCSIController'
+    IDE = 'VirtualIDEController'
+
+    CONTROLLER_TYPE_DICT = {
+        VirtualDiskAdapterType.LSI_LOGIC: LSI_LOGIC,
+        VirtualDiskAdapterType.BUS_LOGIC: BUS_LOGIC,
+        VirtualDiskAdapterType.LSI_LOGIC_SAS: LSI_LOGIC_SAS,
+        VirtualDiskAdapterType.PARA_VIRTUAL: PARA_VIRTUAL,
+        VirtualDiskAdapterType.IDE: IDE}
+
+    @staticmethod
+    def get_controller_type(adapter_type):
+        """Get the disk controller type based on the given adapter type.
+
+        :param adapter_type: disk adapter type
+        :return: controller type corresponding to the given adapter type
+        :raises: InvalidAdapterTypeException
+        """
+        if adapter_type in ControllerType.CONTROLLER_TYPE_DICT:
+            return ControllerType.CONTROLLER_TYPE_DICT[adapter_type]
+        raise ValueError()
+
+    @staticmethod
+    def is_scsi_controller(controller_type):
+        """Check if the given controller is a SCSI controller.
+
+        :param controller_type: controller type
+        :return: True if the controller is a SCSI controller
+        """
+        return controller_type in [ControllerType.LSI_LOGIC,
+                                   ControllerType.BUS_LOGIC,
+                                   ControllerType.LSI_LOGIC_SAS,
+                                   ControllerType.PARA_VIRTUAL]
+
